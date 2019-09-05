3da6b44d376d5a1e1df3fcde2fa51c71492776a2
Alex Essel <thefirmt@gmail.com>
2019-09-04 20:52:37 -0700

    Component style conflicts solved, all reasonable components added.

--- server/server.js
+++ server/server.js
@@ -40,5 +40,16 @@ app.get('/api/reviews/host/:id', (req, res) => {
   });
 })
 
+app.get('/api/images/:id', (req, res) => {
+  let id = req.params.id;
+  axios.get(`http://127.0.0.1:5000/api/images/${id}`)
+  .then(function (response) {
+    res.send(response.data)
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+})
+
 
 app.listen(port, ()=> console.log(`listening on port ${port}!`))
